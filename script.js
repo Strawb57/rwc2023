8204709ac2112ace38830a20cc34d80e9b03cd96
OBC1977 <theoldbarnclub@btconnect.com>
2023-08-25 22:13:48 +0100

    Now reads from database

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp} from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js';
-import { getDatabase, ref, push} from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js';
+import { getDatabase, ref, push, onValue} from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js';
 
 const appSettings = {
     databaseURL: "https://points-9e12e-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -11,8 +11,9 @@ const database = getDatabase(app);
 const pointsDB = ref(database, "Entries");
 
 const inputName = document.getElementById("input-name");
-const inputPoints = document.getElementById("input-points")
+const inputPoints = document.getElementById("input-points");
 const addButton = document.getElementById("add-button");
+const entryListEL = document.getElementById("entry-list");
 
 const member = { name: null, score: null};
 
@@ -25,6 +26,46 @@ addButton.addEventListener("click", function(){
 
     push(pointsDB, member);
 
+    let entry = name + " " + points;
+
+    //updateLineItem(entry);
+
+    clearInputFields();
+
+    onValue( pointsDB, function(snapshot){
+        let entryArray = Object.values(snapshot.val());
+
+        for (let i=0; i < entryArray.length; i++){
+            console.log(entryArray[i]);
+            let currentEntry = Object.entries(entryArray[i]);
+            // delete currentEntry.name;
+            // delete currentEntry.score;
+            //const itemArr = currentEntry.
+            updateLineItem(currentEntry);
+        }
+    
+    })
+
     // console.log(name);
     // console.log(points);
 })
+
+function clearInputFields(){
+    inputName.value = "";
+    inputPoints.value = "";
+}
+
+function updateLineItem(item){
+    entryListEL.innerHTML += `<li>${item}</li>`;
+}
+
+function removeLineItems(){
+    let ul = document.getElementById("entry-list");
+    ul.innerHTML = "";
+
+
+}
+
+function updateAllLineItems(){
+
+}
